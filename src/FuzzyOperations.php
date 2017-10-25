cfd74c1ae91c1faac0770d30076dddeabf22cbd1
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2017-10-25 04:51:20 +0300

    added clas files

---
+++ src/FuzzyOperations.php
@@ -0,0 +1,29 @@
+<?php namespace Bahadircyildiz\PHPFuzzy;
+
+    /**
+    *  A sample class
+    *
+    *  Use this section to define what this class is doing, the PHPDocumentator will use this
+    *  to automatically generate an API documentation using this information.
+    *
+    *  @author Bahadir Can Yildiz
+    */
+class FuzzyOperations {
+
+    /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
+    
+    /**
+    * Sample method 
+    *
+    * Always create a corresponding docblock for each method, describing what it is for,
+    * this helps the phpdocumentator to properly generator the documentation
+    *
+    * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
+    *
+    * @return string
+    */
+
+    public function sum(FuzzyNumber $a, FuzzyNumber $b){
+        
+    }
+}
